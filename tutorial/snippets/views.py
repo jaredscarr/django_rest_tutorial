c8611e48a876360f6269f84617bd7537cb7b2ffa
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-23 07:26:22 -0700

    urls.py in snippets updated. Coresponding views added format param.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,7 +6,7 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """List all snippets, or create a new snippet."""
     if request.method == 'GET':
         snippets = Snippet.objects.all()
@@ -22,7 +22,7 @@ def snippet_list(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """Retrieve, update or delete a snippet instance."""
     try:
         snippet = Snippet.objects.get(pk=pk)
