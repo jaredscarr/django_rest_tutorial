9df11cfd5a058108a0a84677b84580bf09c051d8
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-24 10:56:52 -0700

    updated serializers and the urls to reflect those additions.

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
+from snippets.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
@@ -10,28 +12,27 @@ class SnippetSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
 
-# class SnippetSerializer(serializers.Serializer):
-#     pk = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-#     code = serializers.CharField(style={'base_template': 'textarea.html'})
-#     linenos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class UserSerializer(serializers.ModelSerializer):
+    """User serializer."""
 
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return Snippet.objects.create(**validated_data)
+    snippets = serializers.PrimaryKeyRelatedField(many=True,
+                                                  queryset=Snippet.objects.all()
+                                                  )
 
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.code)
-#         instance.linenos = validated_data.get('linenos', instance.linenos)
-#         instance.language = validated_data.get('language', instance.language)
-#         instance.style = validated_data.get('style', instance.style)
-#         instance.save()
-#         return instance
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
+
+
+class UserList(generics.ListAPIView):
+    """Read only generic based views."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """Read only generic based views."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
