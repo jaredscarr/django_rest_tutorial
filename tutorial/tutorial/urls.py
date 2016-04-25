be8e7b60cea40e0b000a1af15452046a214ee91f
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-24 19:35:13 -0700

    rewired views to use ViewSets and a router.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from snippets import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
